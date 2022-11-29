be3e66c048ba87ea0645cf3d1c672868fba25142
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-11-29 02:57:08 -0600

    split into components + user folder data from trpc

--- src/components/ImageLibrary/Images.tsx
+++ src/components/ImageLibrary/Images.tsx
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { trpc } from "../../utils/trpc";
 import { useSession } from "next-auth/react";
+import UploadedImage from "./UploadedImage";
+import { type Image } from "@prisma/client";
+import EditImageModal from "./EditImageModal";
 
 function Images() {
   // should eventually use status to show something while fetching whether user
   // is logged in or not
   const { data: session, status } = useSession();
 
-  const { data: images, isLoading } = trpc.images.getUserImages.useQuery();
+  const { data: images, isLoading: isLoadingImages } =
+    trpc.images.getUserImages.useQuery();
+  const { data: folders, isLoading: isLoadingFolders } =
+    trpc.images.getUserFolders.useQuery();
 
-  if (isLoading) return <div>Fetching images...</div>;
+  const [imageBeingEdited, setImageBeingEdited] = useState<Image>();
+
+  if (isLoadingImages || isLoadingFolders) return <div>Fetching images...</div>;
+
+  // should probably also use this for showing the folders area, obv don't forget back arrow navigation
+  // to here
 
   return (
     <div>
-      {images?.map((image) => {
-        return (
-          <div key={image.id}>
-            {/* make this a component */}
-            <img
-              src={image.s3ImageURL}
-              alt={image?.title ?? "uploaded image"}
-            />
-          </div>
-        );
-      })}
+      {folders && <div>folders coming soon!</div>}
+      {images && (
+        <div className="grid grid-cols-5 grid-rows-4 gap-4">
+          {images.map((image) => {
+            return (
+              <UploadedImage
+                image={image}
+                key={image.id}
+                setImageBeingEdited={setImageBeingEdited}
+              />
+            );
+          })}
+        </div>
+      )}
+
+      {imageBeingEdited && (
+        <EditImageModal
+          image={imageBeingEdited}
+          setImageBeingEdited={setImageBeingEdited}
+        />
+      )}
     </div>
   );
 }
