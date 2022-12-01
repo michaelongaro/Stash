53ee5abd0c00ac4adfb963129420d8c206c6aa84
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-01 16:18:22 -0600

    also moves files state up like on <Homepage />

--- src/components/ImageLibrary/ImageLibrary.tsx
+++ src/components/ImageLibrary/ImageLibrary.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import DragAndDrop from "../ImageUpload/DragAndDrop";
+import React, { useState } from "react";
+import DragAndDrop, { type IImage } from "../ImageUpload/DragAndDrop";
 import Navbar from "../navbar/Navbar";
 import Images from "./Images";
 
 function ImageLibrary() {
+  const [files, setFiles] = useState<IImage[]>([]);
+
   return (
     <div className="flex flex-col">
       <Navbar />
 
-      <DragAndDrop />
+      <DragAndDrop
+        containerWidth={"100vw"}
+        containerHeight={"85px"}
+        containerBorderRadius={"0"}
+        dragAndDropWidth={"50%"}
+        dragAndDropHeight={"90%"}
+        files={files}
+        setFiles={setFiles}
+        usedInReviewModal={false}
+      />
 
       <Images />
     </div>
