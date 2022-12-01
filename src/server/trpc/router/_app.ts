7773bbf494504eb5bea719f8ce0e842ddb7c6cb6
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-01 16:25:58 -0600

    removed comments

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
-// import { authRouter } from "./auth";
 import { imagesRouter } from "./images";
 
 export const appRouter = router({
   images: imagesRouter,
-  // auth: authRouter,
 });
 
 // export type definition of API
