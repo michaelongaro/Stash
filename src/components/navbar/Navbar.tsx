cf665a6d881c02cf16011d46518dd2a7bf5203be
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-01 16:24:23 -0600

    falls back to localStorage

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,20 @@
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import LogIn from "../auth/LogIn";
 
 function Navbar() {
   const { data: session, status } = useSession();
 
   console.log(status, session);
 
-  if (status === "loading" || !session?.user) {
+  if (
+    (status === "loading" || !session?.user) &&
+    !localStorage.getItem("userID")
+  ) {
     return <main className="flex flex-col items-center pt-4">Loading...</main>;
   }
 
   return (
-    // look into margin shii so that there is a decent gap on sides
     <div className="flex h-14 items-center justify-between bg-slate-400 pl-4 pr-4">
       Stash Logo
       <div className="flex items-center justify-center gap-2">
@@ -25,13 +28,17 @@ function Navbar() {
           height={48}
           style={{ borderRadius: "50%" }}
         />
-        {session?.user.name ?? session?.user.email}
-        <button
-          className="ml-4 rounded-md bg-slate-200 p-2 text-slate-800 transition-colors hover:bg-slate-500 hover:text-slate-200"
-          onClick={() => signOut()}
-        >
-          Log out
-        </button>
+        {session?.user?.name ?? session?.user?.email ?? "New user"}
+        {session?.user?.id ? (
+          <button
+            className="ml-4 rounded-md bg-slate-200 p-2 text-slate-800 transition-colors hover:bg-slate-500 hover:text-slate-200"
+            onClick={() => signOut()}
+          >
+            Log out
+          </button>
+        ) : (
+          <LogIn />
+        )}
       </div>
     </div>
   );
