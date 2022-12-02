5e6d71d9af3292da02fe9dd02da324ff326cfd29
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-02 02:48:45 -0600

    using FaUserCircle instead of img + stash logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
 import LogIn from "../auth/LogIn";
 
 function Navbar() {
@@ -11,33 +12,44 @@ function Navbar() {
     (status === "loading" || !session?.user) &&
     !localStorage.getItem("userID")
   ) {
-    return <main className="flex flex-col items-center pt-4">Loading...</main>;
+    // return <main className="flex flex-col items-center pt-4">Loading...</main>;
+    return <></>;
   }
 
   return (
-    <div className="flex h-14 items-center justify-between bg-slate-400 pl-4 pr-4">
-      Stash Logo
-      <div className="flex items-center justify-center gap-2">
-        <Image
-          src={
-            session?.user?.image ??
-            "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png"
-          }
-          alt={"profile image"}
-          width={48}
-          height={48}
-          style={{ borderRadius: "50%" }}
-        />
-        {session?.user?.name ?? session?.user?.email ?? "New user"}
+    <div className="flex h-14 min-w-[99vw] items-center justify-between bg-blue-600 pl-8 pr-8">
+      <div className="justify-baseline flex items-center gap-[0.2rem] text-4xl text-blue-400">
+        St
+        <div className="translate-y-[4px] rotate-180 text-blue-700">v</div>
+        sh
+      </div>
+      <div className="flex items-center justify-center gap-8">
+        <div className="flex items-center justify-center gap-4">
+          {session?.user?.image ? (
+            <Image
+              src={session?.user?.image}
+              alt={"profile image"}
+              width={48}
+              height={48}
+              style={{ borderRadius: "50%" }}
+            />
+          ) : (
+            <FaUserCircle size={"2rem"} />
+          )}
+          <div className="text-blue-50">
+            {session?.user?.name ?? session?.user?.email ?? "New user"}
+          </div>
+        </div>
         {session?.user?.id ? (
           <button
-            className="ml-4 rounded-md bg-slate-200 p-2 text-slate-800 transition-colors hover:bg-slate-500 hover:text-slate-200"
+            // className="ml-4 rounded-md bg-blue-200 p-2 text-blue-800 transition-colors hover:bg-blue-500 hover:text-blue-200"
+            className="secondaryBtn"
             onClick={() => signOut()}
           >
             Log out
           </button>
         ) : (
-          <LogIn />
+          <LogIn gap={"1rem"} />
         )}
       </div>
     </div>
