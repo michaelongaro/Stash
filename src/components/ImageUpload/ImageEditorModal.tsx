4193b937339d0f0ad5c2ebf7f4c865be789fa341
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-02 02:45:30 -0600

    probably going to replace with react-image-crop

--- src/components/ImageUpload/ImageEditorModal.tsx
+++ src/components/ImageUpload/ImageEditorModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import ImageEditor from "@toast-ui/react-image-editor";
 import { whiteTheme } from "../../ui/whiteTheme";
 
@@ -11,32 +11,47 @@ function ImageEditorModal({
   imageFile,
   setImageToBeEdited,
 }: IImageEditorModal) {
+  const imageEditorRef = useRef();
+
   if (!imageFile) return <></>;
 
   return (
-    <ImageEditor
-      includeUI={{
-        loadImage: {
-          path: URL.createObjectURL(imageFile),
-          name: imageFile.name,
-        },
-        theme: whiteTheme,
-        // menu: ["shape", "filter"],
-        initMenu: "crop",
-        uiSize: {
-          width: "1000px",
-          height: "700px",
-        },
-        menuBarPosition: "right",
-      }}
-      cssMaxHeight={500}
-      cssMaxWidth={700}
-      selectionStyle={{
-        cornerSize: 20,
-        rotatingPointOffset: 70,
-      }}
-      usageStatistics={true}
-    />
+    <div className="absolute top-0 left-0 z-[500] flex min-h-[100vh] min-w-[100vw] items-center justify-center bg-blue-800/90 transition-all">
+      <div className="flex h-full w-full items-center justify-center gap-4">
+        <ImageEditor
+          ref={imageEditorRef}
+          includeUI={{
+            loadImage: {
+              path: URL.createObjectURL(imageFile),
+              name: imageFile.name,
+            },
+            theme: whiteTheme,
+            // menu: ["shape", "filter"],
+            initMenu: "crop",
+            uiSize: {
+              width: "1000px",
+              height: "700px",
+            },
+            menuBarPosition: "right",
+          }}
+          cssMaxHeight={500}
+          cssMaxWidth={700}
+          selectionStyle={{
+            cornerSize: 20,
+            rotatingPointOffset: 70,
+          }}
+          usageStatistics={true}
+        />
+        <button
+          className="secondaryBtn"
+          onClick={() => {
+            // imageEditorRef.current.toDataUrl();
+          }}
+        >
+          Save changes
+        </button>
+      </div>
+    </div>
   );
 }
 
