38a595c7c6f229a7929b5cd0801917bf0a201d63
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-07 16:12:34 -0600

    working image editor, properly returns new File on save

--- src/components/ImageUpload/ImageEditorModal.tsx
+++ src/components/ImageUpload/ImageEditorModal.tsx
@@ -1,54 +1,98 @@
-import React, { useRef } from "react";
-import ImageEditor from "@toast-ui/react-image-editor";
-import { whiteTheme } from "../../ui/whiteTheme";
+import React from "react";
+import FilerobotImageEditor, {
+  TABS,
+  TOOLS,
+} from "react-filerobot-image-editor";
+import { type IFile } from "../ImageUpload/ImageReviewModal";
 
 interface IImageEditorModal {
-  imageFile: File | undefined;
-  setImageToBeEdited: React.Dispatch<React.SetStateAction<File | undefined>>;
+  imageToBeEdited: string | File | undefined;
+  setImageToBeEdited:
+    | React.Dispatch<React.SetStateAction<File | undefined>>
+    | React.Dispatch<React.SetStateAction<string | undefined>>;
+  setEditedImageFile?: React.Dispatch<React.SetStateAction<File | undefined>>;
+  setImageData?: React.Dispatch<React.SetStateAction<IFile[]>>;
+  index?: number;
 }
 
 function ImageEditorModal({
-  imageFile,
+  imageToBeEdited,
+  setEditedImageFile,
   setImageToBeEdited,
+  setImageData,
+  index,
 }: IImageEditorModal) {
-  const imageEditorRef = useRef();
+  // function to turn base64 string into a file
+  function dataURLtoFile(dataurl: string, filename: string) {
+    const arr = dataurl.split(",");
+    const mime = arr[0]!.match(/:(.*?);/)![1];
+    const bstr = atob(arr[1]!);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  }
 
-  if (!imageFile) return <></>;
+  if (!imageToBeEdited) return <></>;
 
   return (
     <div className="absolute top-0 left-0 z-[500] flex min-h-[100vh] min-w-[100vw] items-center justify-center bg-blue-800/90 transition-all">
-      <div className="flex h-full w-full items-center justify-center gap-4">
-        <ImageEditor
-          ref={imageEditorRef}
-          includeUI={{
-            loadImage: {
-              path: URL.createObjectURL(imageFile),
-              name: imageFile.name,
-            },
-            theme: whiteTheme,
-            // menu: ["shape", "filter"],
-            initMenu: "crop",
-            uiSize: {
-              width: "1000px",
-              height: "700px",
-            },
-            menuBarPosition: "right",
+      <div className="relative flex h-full w-[85vw] flex-col items-center justify-center gap-4">
+        <FilerobotImageEditor
+          source={
+            typeof imageToBeEdited === "string"
+              ? imageToBeEdited
+              : URL.createObjectURL(imageToBeEdited)
+          }
+          onBeforeSave={() => false}
+          translations={{
+            save: "Apply changes",
           }}
-          cssMaxHeight={500}
-          cssMaxWidth={700}
-          selectionStyle={{
-            cornerSize: 20,
-            rotatingPointOffset: 70,
+          onSave={(editedImageObject, designState) => {
+            console.log("saved", editedImageObject, designState);
+            if (setEditedImageFile) {
+              setEditedImageFile(
+                dataURLtoFile(editedImageObject.imageBase64!, "editedImage")
+              );
+            } else if (
+              setImageData &&
+              index !== undefined &&
+              typeof imageToBeEdited !== "string"
+            ) {
+              setImageData((oldImageData) => {
+                const newImageData = [...oldImageData];
+                if (newImageData[index]?.image.imageFile) {
+                  newImageData[index]!.image.imageFile = dataURLtoFile(
+                    editedImageObject.imageBase64!,
+                    imageToBeEdited.name
+                  );
+                }
+                return newImageData;
+              });
+            }
+            setImageToBeEdited(undefined);
           }}
-          usageStatistics={true}
+          // avoidChangesNotSavedAlertOnLeave={true} prob uncomment later
+          // showBackButton={true}
+          annotationsCommon={{
+            fill: "#ff0000",
+          }}
+          Text={{ text: "Your text here..." }}
+          Rotate={{ angle: 90, componentType: "slider" }}
+          savingPixelRatio={2} // what do these do
+          previewPixelRatio={2} // what do these do
+          tabsIds={[TABS.ADJUST, TABS.ANNOTATE, TABS.RESIZE]}
+          defaultTabId={TABS.ADJUST}
+          defaultToolId={TOOLS.CROP}
         />
+
         <button
-          className="secondaryBtn"
-          onClick={() => {
-            // imageEditorRef.current.toDataUrl();
-          }}
+          className="dangerBtn absolute top-12 right-2 "
+          onClick={() => setImageToBeEdited(undefined)}
         >
-          Save changes
+          Discard changes
         </button>
       </div>
     </div>
