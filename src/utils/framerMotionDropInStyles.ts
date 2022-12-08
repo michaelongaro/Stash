9b9828e32306be34a33eea583977aa5d174ef424
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-07 22:54:20 -0600

    springy animation for framer motion

---
+++ src/utils/framerMotionDropInStyles.ts
@@ -0,0 +1,20 @@
+export const dropIn = {
+  hidden: {
+    y: "100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.15,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
