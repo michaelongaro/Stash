f3d58567cb8bfb93ce983d751fa3860ff503dc08
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-07 22:52:51 -0600

    init commit

---
+++ src/components/logo/StashLogoAnimation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import classes from "./StashLogoAnimation.module.css";
+
+interface IStashLogoAnimation {
+  size: string;
+}
+
+function StashLogoAnimation({ size }: IStashLogoAnimation) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 156 157"
+      fill="none"
+      className={classes.stashLogo}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.5 144L45.5 78.25L77.5 12.5L142.5 144"
+        stroke="rgb(96 165 250)"
+        stroke-width="25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default StashLogoAnimation;
