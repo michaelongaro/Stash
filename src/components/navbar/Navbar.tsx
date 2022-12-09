322d7f7adb5fc8090037ade1d2aa7ce8bf803fea
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-08 20:16:20 -0600

    responsive styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,24 +6,21 @@ import LogIn from "../auth/LogIn";
 function Navbar() {
   const { data: session, status } = useSession();
 
-  console.log(status, session);
-
   if (
     (status === "loading" || !session?.user) &&
     !localStorage.getItem("userID")
   ) {
-    // return <main className="flex flex-col items-center pt-4">Loading...</main>;
     return <></>;
   }
 
   return (
-    <div className="flex h-14 w-[100vw] items-center justify-between bg-blue-600 pl-8 pr-8">
+    <div className="flex h-14 w-[100vw] max-w-full items-center justify-between bg-blue-600 pl-8 pr-8">
       <div className="justify-baseline flex select-none items-center gap-[0.2rem] text-4xl text-blue-400">
         St
         <div className="translate-y-[4px] rotate-180 text-blue-700">v</div>
         sh
       </div>
-      <div className="flex items-center justify-center gap-8">
+      <div className="flex items-center justify-center gap-4 md:gap-8">
         <div className="flex items-center justify-center gap-4">
           {session?.user?.image ? (
             <Image
@@ -36,16 +33,12 @@ function Navbar() {
           ) : (
             <FaUserCircle size={"2rem"} />
           )}
-          <div className="text-blue-50">
+          <div className="hidden text-blue-50 sm:block">
             {session?.user?.name ?? session?.user?.email ?? "New user"}
           </div>
         </div>
         {session?.user?.id ? (
-          <button
-            // className="ml-4 rounded-md bg-blue-200 p-2 text-blue-800 transition-colors hover:bg-blue-500 hover:text-blue-200"
-            className="secondaryBtn"
-            onClick={() => signOut()}
-          >
+          <button className="secondaryBtn" onClick={() => signOut()}>
             Log out
           </button>
         ) : (
