96075d4648fa7f950f65020278fee7a31ade9ab1
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-08 20:17:08 -0600

    typings + workaround for react-select dropdown

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,10 +1,15 @@
 import { useEffect } from "react";
 
+import { type Image } from "@prisma/client";
+import { type IImage } from "../components/ImageUpload/DragAndDrop";
+
 interface IOnClickOutside {
   ref: React.RefObject<HTMLDivElement>;
   setter:
+    | React.Dispatch<React.SetStateAction<IImage[]>>
+    | React.Dispatch<React.SetStateAction<Image | undefined>>
     | React.Dispatch<React.SetStateAction<boolean>>
-    | React.Dispatch<React.SetStateAction<string>>;
+    | React.Dispatch<React.SetStateAction<string | undefined>>;
   backupSetter?: React.Dispatch<React.SetStateAction<boolean>>;
   hideModalValue: object | boolean | undefined;
   backupHideModalValue?: boolean;
@@ -19,21 +24,23 @@ export default function useOnClickOutside({
   backupHideModalValue,
   useBackupSetter,
 }: IOnClickOutside) {
-  console.log(backupSetter === undefined, backupHideModalValue === undefined);
-
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
       // Do nothing if clicking ref's element or descendent elements
 
-      if (!ref.current || ref.current.contains(event.target as Node)) {
-        console.log("returning");
-
+      if (
+        !ref.current ||
+        ref.current.contains(event.target as Node) ||
+        event.target?.toString() === "[object SVGPathElement]" ||
+        event.target?.toString() === "[object SVGSVGElement]"
+      ) {
         return;
       }
 
       if (useBackupSetter && backupSetter && backupHideModalValue) {
         backupSetter(backupHideModalValue);
       } else {
+        // @ts-expect-error - unsure of how to create a generic type for this
         setter(hideModalValue);
       }
     };
