f9aa54311ebc131f436eb715f7a8e714be633371
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-08 20:18:04 -0600

    since modals are fixed, no longer scrolls to top

--- src/hooks/useScrollModalIntoView.ts
+++ src/hooks/useScrollModalIntoView.ts
@@ -2,11 +2,6 @@ import { useEffect } from "react";
 
 export default function useScrollModalIntoView() {
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
     document.body.style.overflow = "hidden";
 
     return () => {
