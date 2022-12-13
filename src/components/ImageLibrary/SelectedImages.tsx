cb39c177797d7475707116f1eaed0c1176393a7b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-13 15:28:06 -0600

    mobile styling + ConfirmDeleteModal

--- src/components/ImageLibrary/SelectedImages.tsx
+++ src/components/ImageLibrary/SelectedImages.tsx
@@ -3,9 +3,10 @@ import CreateSelectable from "react-select/creatable";
 import { FaCheck, FaFolderOpen, FaTimes } from "react-icons/fa";
 import { trpc } from "../../utils/trpc";
 import { type IFolderOptions } from "../ImageUpload/ImageReviewModal";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useLocalStorageContext } from "../../context/LocalStorageContext";
 import { useSession } from "next-auth/react";
+import ConfirmDeleteModal from "../modals/ConfirmDeleteModal";
 
 interface ISelectedImages {
   selectedImageIDs: string[];
@@ -28,6 +29,8 @@ function SelectedImages({
     useState<IFolderOptions | null>(null);
 
   const [showCreateSelectableDropdown, setShowCreateSelectableDropdown] =
+    useState<boolean>(false);
+  const [showConfirmDeleteModal, setShowConfirmDeleteModal] =
     useState<boolean>(false);
 
   const [newlyAddedFolderID, setNewlyAddedFolderID] = useState<string>();
@@ -83,26 +86,6 @@ function SelectedImages({
       },
     });
 
-  const deleteSelectedImages = trpc.images.deleteSelectedImages.useMutation({
-    onMutate: () => {
-      utils.images.getUserImages.cancel();
-      const optimisticUpdate = utils.images.getUserImages.getData();
-
-      if (optimisticUpdate) {
-        utils.images.getUserImages.setData(optimisticUpdate);
-      }
-    },
-    onSuccess() {
-      setShowCreateSelectableDropdown(false);
-      setSelectedImageIDs([]);
-    },
-    onSettled: () => {
-      utils.images.getUserImages.invalidate();
-      setShowCreateSelectableDropdown(false); // needed?
-      setSelectedImageIDs([]);
-    },
-  });
-
   // probably redo these two effects later
   useEffect(() => {
     if (newlyAddedFolderID && newlyAddedFolderID.length > 0) {
@@ -144,114 +127,132 @@ function SelectedImages({
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.15 }}
-      className="m-6 flex flex-col items-center justify-center gap-2 rounded-md  bg-blue-600 p-2 sm:flex-row sm:items-center sm:justify-end"
+      className="m-6 flex flex-col items-center justify-center gap-4 rounded-md  bg-blue-600 p-2 sm:flex-row sm:items-center sm:justify-end"
     >
       {`${selectedImageIDs.length} image${
         selectedImageIDs.length > 1 ? "s" : ""
       } selected`}
 
-      <button
-        style={{
-          display: showCreateSelectableDropdown ? "none" : "flex",
-        }}
-        className="secondaryBtn flex items-center justify-center gap-2"
-        onClick={() => setShowCreateSelectableDropdown(true)}
-      >
-        <FaFolderOpen size={"1rem"} />
-        Move to folder
-      </button>
+      <div className="flex flex-col items-center justify-center gap-2 sm:flex-row sm:items-center sm:justify-end">
+        <button
+          style={{
+            display: showCreateSelectableDropdown ? "none" : "flex",
+          }}
+          className="secondaryBtn flex items-center justify-center gap-2"
+          onClick={() => setShowCreateSelectableDropdown(true)}
+        >
+          <FaFolderOpen size={"1rem"} />
+          Move to folder
+        </button>
 
-      {showCreateSelectableDropdown && (
-        <div className="flex flex-col items-end justify-center gap-2 sm:flex-row sm:items-center sm:justify-end">
-          <CreateSelectable
-            isClearable
-            styles={{
-              option: (baseStyles, state) => ({
-                ...baseStyles,
-                color: "#1e3a8a",
-              }),
-            }}
-            formatCreateLabel={(inputValue) => `Create folder "${inputValue}"`}
-            options={folderOptions}
-            onChange={(newFolder) => {
-              if (newFolder?.label) {
-                // updating list of folders in dropdown
-                if (
-                  folderOptions.every((elem) => elem.label !== newFolder.label)
-                ) {
-                  // making sure folder isn't already present)
-                  const newFolderOptions = [...folderOptions];
-                  newFolderOptions[newFolderOptions.length] = {
+        {showCreateSelectableDropdown && (
+          <div className="flex flex-col items-end justify-center gap-2 sm:flex-row sm:items-center sm:justify-end">
+            <CreateSelectable
+              isClearable
+              styles={{
+                option: (baseStyles, state) => ({
+                  ...baseStyles,
+                  color: "#1e3a8a",
+                }),
+              }}
+              formatCreateLabel={(inputValue) =>
+                `Create folder "${inputValue}"`
+              }
+              options={folderOptions}
+              onChange={(newFolder) => {
+                if (newFolder?.label) {
+                  // updating list of folders in dropdown
+                  if (
+                    folderOptions.every(
+                      (elem) => elem.label !== newFolder.label
+                    )
+                  ) {
+                    // making sure folder isn't already present)
+                    const newFolderOptions = [...folderOptions];
+                    newFolderOptions[newFolderOptions.length] = {
+                      label: newFolder.label,
+                      value: newFolder.value,
+                    };
+                    setFolderOptions(newFolderOptions);
+                  }
+
+                  setCurrentlySelectedFolder({
                     label: newFolder.label,
                     value: newFolder.value,
-                  };
-                  setFolderOptions(newFolderOptions);
+                  });
+                } else {
+                  // want to uncomment below once you have functionality to fully delete folder from this menu
+                  // (assuming you want that functionality)
+                  // const newFolderOptions = [...folderOptions];
+                  // delete newFolderOptions[index];
+                  // setFolderOptions(newFolderOptions);
+
+                  setCurrentlySelectedFolder(null);
                 }
-
-                setCurrentlySelectedFolder({
-                  label: newFolder.label,
-                  value: newFolder.value,
-                });
-              } else {
-                // want to uncomment below once you have functionality to fully delete folder from this menu
-                // (assuming you want that functionality)
-                // const newFolderOptions = [...folderOptions];
-                // delete newFolderOptions[index];
-                // setFolderOptions(newFolderOptions);
-
-                setCurrentlySelectedFolder(null);
-              }
-            }}
-            value={currentlySelectedFolder}
-            placeholder="Destination folder"
-          />
-
-          <div className="flex items-center justify-center gap-1">
-            <button
-              className="secondaryBtn"
-              onClick={() => {
-                setShowCreateSelectableDropdown(false);
               }}
-            >
-              <FaTimes size={"1rem"} />
-            </button>
-            <button
-              className="secondaryBtn"
-              disabled={!currentlySelectedFolder}
-              onClick={() => {
-                if (currentlySelectedFolder) {
-                  if (
-                    currentlySelectedFolder &&
-                    currentlySelectedFolder.value ===
-                      currentlySelectedFolder.label &&
-                    (localStorageID?.value || session?.user?.id)
-                  ) {
-                    createFolder.mutate({
-                      title: currentlySelectedFolder.label,
-                      userID: localStorageID?.value ?? session?.user?.id ?? "",
-                    });
-                  } else {
-                    setReadyToUpdate(true);
+              value={currentlySelectedFolder}
+              placeholder="Destination folder"
+            />
+
+            <div className="flex items-center justify-center gap-1">
+              <button
+                className="secondaryBtn"
+                onClick={() => {
+                  setShowCreateSelectableDropdown(false);
+                }}
+              >
+                <FaTimes size={"1rem"} />
+              </button>
+              <button
+                className="secondaryBtn"
+                disabled={!currentlySelectedFolder}
+                onClick={() => {
+                  if (currentlySelectedFolder) {
+                    if (
+                      currentlySelectedFolder &&
+                      currentlySelectedFolder.value ===
+                        currentlySelectedFolder.label &&
+                      (localStorageID?.value || session?.user?.id)
+                    ) {
+                      createFolder.mutate({
+                        title: currentlySelectedFolder.label,
+                        userID:
+                          localStorageID?.value ?? session?.user?.id ?? "",
+                      });
+                    } else {
+                      setReadyToUpdate(true);
+                    }
                   }
-                }
-              }}
-            >
-              <FaCheck size={"1rem"} />
-            </button>
+                }}
+              >
+                <FaCheck size={"1rem"} />
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      <button
-        className="dangerBtn"
-        onClick={() => {
-          if (currentlySelectedFolder) {
-            deleteSelectedImages.mutate({
-              idsToUpdate: selectedImageIDs,
-            });
-          }
-        }}
-      >{`Delete${selectedImageIDs.length > 1 ? " all" : ""}`}</button>
+        <button
+          className="dangerBtn"
+          onClick={() => {
+            setShowConfirmDeleteModal(true);
+          }}
+        >{`Delete${selectedImageIDs.length > 1 ? " all" : ""}`}</button>
+      </div>
+
+      <AnimatePresence
+        initial={false}
+        mode={"wait"}
+        onExitComplete={() => null}
+      >
+        {showConfirmDeleteModal && (
+          <ConfirmDeleteModal
+            type={"images"}
+            setShowConfirmDeleteModal={setShowConfirmDeleteModal}
+            idsToDelete={selectedImageIDs}
+            afterBulkImageDeletionCallback={setSelectedImageIDs}
+          />
+        )}
+      </AnimatePresence>
     </motion.div>
   );
 }
