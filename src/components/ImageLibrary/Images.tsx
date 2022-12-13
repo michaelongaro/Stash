cd1ca553160b62df4aa070283d0ee02774041669
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-13 15:27:13 -0600

    uses <Folder /> component

--- src/components/ImageLibrary/Images.tsx
+++ src/components/ImageLibrary/Images.tsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { trpc } from "../../utils/trpc";
 import { useSession } from "next-auth/react";
-import UploadedImage from "./UploadedImage";
-import { type Image, type Folder } from "@prisma/client";
-import EditImageModal from "./EditImageModal";
-import {
-  FaFolder,
-  FaHome,
-  FaEdit,
-  FaTrash,
-  FaTimes,
-  FaCheck,
-} from "react-icons/fa";
 import { AnimatePresence } from "framer-motion";
-import SelectedImages from "./SelectedImages";
 import { useLocalStorageContext } from "../../context/LocalStorageContext";
+import SelectedImages from "./SelectedImages";
+import UploadedImage from "./UploadedImage";
+import Folders from "./Folders";
+import EditImageModal from "./EditImageModal";
+import { type Image, type Folder } from "@prisma/client";
 
 function Images() {
   // should eventually use status to show something while fetching whether user
@@ -22,7 +15,6 @@ function Images() {
   const localStorageID = useLocalStorageContext();
 
   const { data: session, status } = useSession();
-  const utils = trpc.useContext();
 
   const { data: userImages, isLoading: isLoadingImages } =
     trpc.images.getUserImages.useQuery(
@@ -43,50 +35,7 @@ function Images() {
   const [imageBeingEdited, setImageBeingEdited] = useState<Image>();
   const [imagesToShow, setImagesToShow] = useState<Image[]>([]);
 
-  const [editingFolderData, setEditingFolderData] = useState<boolean>(false);
-  const [temporaryFolderData, setTemporaryFolderData] = useState<Folder | null>(
-    null
-  );
-
   const [selectedImages, setSelectedImages] = useState<string[]>([]);
-
-  const updateFolderData = trpc.folders.updateFolderData.useMutation({
-    onMutate: () => {
-      utils.folders.getUserFolders.cancel();
-      const optimisticUpdate = utils.folders.getUserFolders.getData();
-
-      if (optimisticUpdate) {
-        utils.folders.getUserFolders.setData(optimisticUpdate);
-      }
-    },
-    onSuccess() {
-      setSelectedFolder(temporaryFolderData);
-      setEditingFolderData(false);
-    },
-    onSettled: () => {
-      utils.folders.getUserFolders.invalidate();
-      setImageBeingEdited(undefined);
-    },
-  });
-
-  const deleteFolder = trpc.folders.deleteFolder.useMutation({
-    onMutate: () => {
-      utils.folders.getUserFolders.cancel();
-      const optimisticUpdate = utils.folders.getUserFolders.getData();
-
-      if (optimisticUpdate) {
-        utils.folders.getUserFolders.setData(optimisticUpdate);
-      }
-    },
-    onSuccess() {
-      utils.folders.getUserFolders.invalidate();
-      setSelectedFolder(null);
-    },
-    onSettled: () => {
-      utils.folders.getUserFolders.invalidate();
-      setImageBeingEdited(undefined);
-    },
-  });
 
   useEffect(() => {
     if (imagesFromSelectedFolder !== undefined && selectedFolder) {
@@ -96,139 +45,17 @@ function Images() {
     }
   }, [userImages, selectedFolder, imagesFromSelectedFolder]);
 
-  useEffect(() => {
-    setTemporaryFolderData(selectedFolder); // maybe a problem?
-  }, [selectedFolder]);
-
   if (isLoadingImages || isLoadingFolders) return <></>;
 
   return (
     <div className="mt-2 w-[85vw] pb-[15%]">
       {folders && userImages && (
-        <div className="rounded-md bg-blue-500 p-2">
-          {selectedFolder ? (
-            <div className="flex flex-col items-center justify-start gap-4 sm:flex-row">
-              <button
-                className="secondaryBtn flex items-center justify-center gap-2"
-                onClick={() => {
-                  setSelectedFolder(null);
-                }}
-              >
-                <FaHome size={"1rem"} />
-                Back to home
-              </button>
-
-              <div className="flex items-center justify-center gap-4">
-                <div className="flex items-center justify-center gap-2">
-                  {editingFolderData ? (
-                    <div className="flex flex-col items-center justify-center gap-2 sm:flex-row">
-                      <div className="flex items-center justify-center gap-2">
-                        <FaFolder size={"1rem"} />
-                        <input
-                          className="w-full rounded-md pl-2"
-                          type="text"
-                          placeholder="Title"
-                          value={temporaryFolderData?.title ?? "Loading..."}
-                          onChange={(e) => {
-                            const newFolderData = { ...selectedFolder };
-                            newFolderData.title = e.target.value;
-                            setTemporaryFolderData(newFolderData);
-                          }}
-                        />
-                      </div>
-
-                      <div className="flex items-center justify-center gap-2">
-                        <button
-                          className="secondaryBtn"
-                          onClick={() => {
-                            const newFolderData = { ...selectedFolder };
-                            newFolderData.title = selectedFolder.title;
-                            setTemporaryFolderData(newFolderData);
-                            setEditingFolderData(false);
-                          }}
-                        >
-                          <FaTimes size={"1rem"} />
-                        </button>
-                        <button
-                          className="secondaryBtn"
-                          onClick={() => {
-                            // call update folder mutation here
-                            if (temporaryFolderData) {
-                              updateFolderData.mutate({
-                                id: temporaryFolderData.id,
-                                title: temporaryFolderData.title,
-                                description: temporaryFolderData?.description,
-                              });
-                            }
-                          }}
-                        >
-                          <FaCheck size={"1rem"} />
-                        </button>
-                      </div>
-
-                      <button
-                        className="dangerBtn"
-                        onClick={() =>
-                          deleteFolder.mutate({
-                            id: selectedFolder.id,
-                          })
-                        }
-                      >
-                        <FaTrash size={"1rem"} />
-                      </button>
-                    </div>
-                  ) : (
-                    <div className="flex items-center justify-center gap-4">
-                      <FaFolder size={"1rem"} />
-
-                      <div>{selectedFolder.title}</div>
-                      <button
-                        className="secondaryBtn"
-                        onClick={() => setEditingFolderData(true)}
-                      >
-                        <FaEdit size={"1rem"} />
-                      </button>
-
-                      <button
-                        className="dangerBtn"
-                        onClick={() =>
-                          deleteFolder.mutate({
-                            id: selectedFolder.id,
-                          })
-                        }
-                      >
-                        <FaTrash size={"1rem"} />
-                      </button>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-          ) : (
-            <>
-              <div className="pl-2 text-xl">Folders</div>
-              <div className="flex flex-wrap items-center justify-start gap-4 ">
-                {folders.map((folder) => {
-                  return (
-                    <button
-                      key={folder.id}
-                      className="secondaryBtn flex items-center justify-center gap-2"
-                      onClick={() => {
-                        setSelectedFolder(folder);
-                      }}
-                      onMouseEnter={() => {
-                        // setHovering             probably end up extracting this to sep. component so you can have just one state like this
-                      }}
-                    >
-                      <FaFolder size={"1rem"} />
-                      {folder.title}
-                    </button>
-                  );
-                })}
-              </div>
-            </>
-          )}
-        </div>
+        <Folders
+          folders={folders}
+          selectedFolder={selectedFolder}
+          setSelectedFolder={setSelectedFolder}
+          setImageBeingEdited={setImageBeingEdited}
+        />
       )}
 
       <AnimatePresence
@@ -246,7 +73,7 @@ function Images() {
 
       {imagesToShow && (
         <div className="m-6 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
-          {imagesToShow.map((image, index) => {
+          {imagesToShow.map((image) => {
             return (
               <UploadedImage
                 key={image.id}
