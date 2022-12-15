c4fe21e00904415d7bb4d685b9120b60f21e5ad0
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-14 22:16:22 -0600

    HomePage -> own folder + sliding word animation

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { type IImage } from "../ImageUpload/DragAndDrop";
+
+import LogIn from "../auth/LogIn";
+import DragAndDrop from "../ImageUpload/DragAndDrop";
+
+import classes from "./HomePage.module.css";
+
+function HomePage() {
+  const [files, setFiles] = useState<IImage[]>([]);
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center gap-8 rounded-md bg-blue-200 p-4">
+        <h1 className="justify-baseline flex items-center gap-[0.2rem] text-5xl text-blue-400">
+          St
+          <div className="translate-y-[5px] rotate-180 text-blue-700">v</div>
+          sh
+        </h1>
+
+        <h2 className="flex flex-col items-center justify-center gap-2 text-blue-500 sm:flex-row">
+          your personal vault for all of your
+          <div className="flex items-center justify-center gap-2 ">
+            <div className="h-[1.5rem] w-[92px] overflow-hidden">
+              <ul className={classes.tickerList}>
+                <li>
+                  <div>family</div>
+                </li>
+                <li>
+                  <div>private</div>
+                </li>
+                <li>
+                  <div>vacation</div>
+                </li>
+                <li>
+                  <div>homework</div>
+                </li>
+              </ul>
+            </div>
+            <div className="text-blue-500">photos</div>
+          </div>
+        </h2>
+      </div>
+
+      <LogIn gap={"1.5rem"} />
+
+      <DragAndDrop
+        renderedLocation={"homepage"}
+        files={files}
+        setFiles={setFiles}
+        usedInReviewModal={false}
+      />
+    </div>
+  );
+}
+
+export default HomePage;
