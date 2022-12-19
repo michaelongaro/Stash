637543bbb28f28ab2e42c130ecced83f0d66ac88
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-19 17:02:40 -0600

    placeholderRouter no longer exists

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -3,14 +3,12 @@ import { foldersRouter } from "./folders";
 import { imagesRouter } from "./images";
 import { usersRouter } from "./users";
 import { metadataRouter } from "./metadata";
-import { placeholderRouter } from "./placeholder";
 
 export const appRouter = router({
   images: imagesRouter,
   users: usersRouter,
   folders: foldersRouter,
   metadataRouter: metadataRouter,
-  placeholderRouter: placeholderRouter,
 });
 
 // export type definition of API
